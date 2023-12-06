8feb3950fb7f73a686cfd7794b56eada224b1eb0
jaime1098 <145287450+jaime1098@users.noreply.github.com>
2023-12-06 15:06:43 +0000

    First steps

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -12,9 +12,10 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdarg.h>
+# include <fcntl.h>	
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
 
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 32
