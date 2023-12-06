76555c3b575a2374251841b01e8fe3b9b6ee563a
jaime1098 <145287450+jaime1098@users.noreply.github.com>
2023-12-06 15:05:05 +0000

    First steps

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -1,47 +1,59 @@
-#include <stdlib.h>
-#include <unistd.h>
+#include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
-	char	*buffer;
-	char	*line;
-	size_t	lenght;
-	int		i;
-	int		j;
+	static char *buffer;
+	char *line;
+	size_t length;
+	int i;
+	int j;
 
-	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	length = 1;
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
 		return (NULL);
-	line = NULL;
-	lenght = read(fd, buffer, BUFFER_SIZE);
-	if (lenght < 0) // Error in read
-		return (NULL);
-	while (lenght > 0)
-	{
-		buffer[lenght] = '\0'; // Null-terminate the buffer
-		i = 0;
-		while (buffer)
+	while (length > 0)
+		length = read(fd, buffer, BUFFER_SIZE);
+		if ((int)length < 0)
 		{
-			// Find newline character
-			if (buffer[i] == '\n')
-			{
-				// Allocate memory for line and copy content
-				line = (char *)malloc((i + 1) * sizeof(char));
-				if (!line)
-				{
-					free(buffer);
-					return (NULL);
-				}
-				j = 0;
-				// Copy the line into the allocated memory
-				while (j++ = < i)
-					line[j] = buffer[j];
-				line[i] = '\0'; // Null-terminate the line
-				return (line);
-			}
-			i++;
+			free(buffer);
+			return (NULL);
 		}
+	printf("%s\n", buffer);
+	i = 0;
+	while (buffer[i] != '\0')
+	{
+		j = 0;
+		line = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		while (buffer[i] != '\n' && buffer [i] != '\0')
+			line[j++] = buffer[i++];
+		line[j++] = '\n';
+		line[j] = '\0';
+		printf("%s\n", line);
+		free (line);
+		i++;
 	}
-	free(buffer);
-	return (NULL);
+	return(line);
+}
+
+void	leaks(void)
+{
+	system("leaks -q a.out");
+}
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+	int		lines;
+
+	lines = 1;
+	fd = open("texto.txt", O_RDONLY);
+	line = get_next_line(fd);
+	free(line);
+	atexit(leaks);
+	close(fd);
+	return (0);
 }
